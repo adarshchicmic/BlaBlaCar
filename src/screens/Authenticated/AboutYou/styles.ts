6e6c0efcc4fec789c937bb37f355682b3c082738
Adarsh Pandey <adarsh.pandey@chicmic.co.in>
2023-05-31 10:31:28 +0530

    fix: borderRadiusChange in About you page

--- src/screens/Authenticated/AboutYou/styles.ts
+++ src/screens/Authenticated/AboutYou/styles.ts
@@ -47,7 +47,7 @@ const styles = StyleSheet.create<any>({
   imageStyle: {
     height: hp(15),
     width: wp(30),
-    borderRadius: '50%',
+    borderRadius: 60,
   },
   buttonTextStyle: {
     color: '#2dbeff',
